f22e335b00a6ed4de481d142f1062b2c2ea79ce5
M-OssaOliveira <molekamari@gmail.com>
2025-12-26 14:47:00 -0300

    aula78 - criando e mapeando a entidade Cliente / aula 79 Dto e mapper para o recurso cliente

---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/dto/mapper/ClienteMapper.java
@@ -0,0 +1,22 @@
+package com.mballem.demoparkapi.web.dto.mapper;
+
+import com.mballem.demoparkapi.entity.Cliente;
+import com.mballem.demoparkapi.web.dto.ClienteCreateDto;
+import com.mballem.demoparkapi.web.dto.ClienteResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+public class ClienteMapper {
+
+    private static Cliente toCliente (ClienteCreateDto dto){
+        return new ModelMapper().map(dto, Cliente.class);
+    }
+
+    private static ClienteResponseDto toDto (Cliente cliente){
+        return new ModelMapper().map(cliente, ClienteResponseDto.class);
+    }
+}

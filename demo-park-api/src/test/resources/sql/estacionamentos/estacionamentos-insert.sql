43acc0d0db8119b24d942127069e4c45d6515a68
M-OssaOliveira <molekamari@gmail.com>
2026-01-02 17:42:07 -0300

    inicio aula 109 - começando com os arquivos de testes do estacionamento

---
+++ demo-park-api/src/test/resources/sql/estacionamentos/estacionamentos-insert.sql
@@ -0,0 +1,11 @@
+insert into USUARIOS (id, username, password, role) values (100,'ana@email.com',
+'$2a$12$Z/YMDTy3br.Cmq.yu0EpM.iUODoJVlb1ioQCe3TvD4f5pbiCrAZWi','ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101,'bia@email.com',
+'$2a$12$Z/YMDTy3br.Cmq.yu0EpM.iUODoJVlb1ioQCe3TvD4f5pbiCrAZWi','ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102,'bob@email.com',
+'$2a$12$Z/YMDTy3br.Cmq.yu0EpM.iUODoJVlb1ioQCe3TvD4f5pbiCrAZWi','ROLE_CLIENTE');
+
+insert into VAGAS (id, codigo, status_vaga) values (10,'A-01','LIVRE');
+insert into VAGAS (id, codigo, status_vaga) values (20,'A-02','LIVRE');
+insert into VAGAS (id, codigo, status_vaga) values (30,'A-03','OCUPADA');
+insert into VAGAS (id, codigo, status_vaga) values (40,'A-04','LIVRE');
